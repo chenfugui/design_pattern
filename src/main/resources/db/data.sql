e8d736da4190d14c6119ba14e28bda84bda12607
chenfugui <2801090484@qq.com>
2020-09-23 23:05:13 +0800

    添加h2数据库

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,2 @@
+insert into student(id,name,age,address) values(1,'zhangsan',8,'beijingshi');
+insert into teacher(id,name,age,address) values(1,'wangdaniu',8,'niuyue');
---
+++ src/main/resources/db/schema.sql
@@ -0,0 +1,2 @@
+create TABLE student (id int,name varchar ,age int ,address varchar ,createtime date);
+create TABLE teacher(id int,name varchar ,age int ,address varchar ,createtime date);
